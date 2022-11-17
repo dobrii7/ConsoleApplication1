58fd6dcf7b22330e8f34d123ffff3bcbc61f76d8
Дмитрий Шентяпин <dobrii7@hotmail.com>
2022-11-17 21:12:45 +0700

    thread!

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+void doWord()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "id-thread  " << std::this_thread::get_id() << "  Do Word " << i << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n" << std::endl;
+	//doWord();
+	std::thread dow(doWord);
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "id-thread  " << std::this_thread::get_id() << "  main " << i << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+	dow.join();
+	return 0;
 }
