d98b8705e3a317778ac8a323fbb881297c3ae84f
Дмитрий Шентяпин <dobrii7@hotmail.com>
2022-11-21 22:32:03 +0700

    thread porametr

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,12 +2,12 @@
 #include <thread>
 #include <chrono>
 
-void doWord()
+void doWord(int a)
 {
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < a; i++)
 	{
 		std::cout << "id-thread  " << std::this_thread::get_id() << "  Do Word " << i << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+		std::this_thread::sleep_for(std::chrono::milliseconds(150));
 	}
 }
 
@@ -15,11 +15,11 @@ int main()
 {
 	std::cout << "Hello World!\n" << std::endl;
 	//doWord();
-	std::thread dow(doWord);
+	std::thread dow(doWord, 20);
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << "id-thread  " << std::this_thread::get_id() << "  main " << i << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 	dow.join();
 	return 0;
